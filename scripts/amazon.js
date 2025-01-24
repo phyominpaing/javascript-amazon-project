715b6886928dbf34463d4a008be4ad7da25a751c
phyominpaing <minpaingphyo255@gmail.com>
2025-01-24 23:17:18 +0630

    Day 3 Add to Cart Btn Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,4 @@
-
-let productsHTML = ''
+let productsHTML = "";
 
 products.forEach((product) => {
   productsHTML += `
@@ -47,13 +46,38 @@ products.forEach((product) => {
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart" 
+        data-product-id = '${product.id}'>
           Add to Cart
         </button>
     </div>
     `;
-
-    
 });
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (item.productId === productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+    
+  });
+});
