91547f108297540f4217de2079e99facd3075081
phyominpaing <minpaingphyo255@gmail.com>
2025-01-25 23:37:13 +0630

    Day 4 Start Separating Module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import { cart } from "../data/cart.js";
+
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -25,7 +27,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class='js-quantity-selector-${product.id}'>
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -41,7 +43,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -56,9 +58,13 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", () => { 
     const productId = button.dataset.productId;
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`).value;
+    const quantity = Number(quantitySelector);
 
     let matchingItem;
 
@@ -68,16 +74,31 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       }
     });
 
+    console.log(matchingItem);
+    
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity: 1,
+        productId,
+        quantity,
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
     
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`)
+
+    addedMessage.classList.add("added-to-cart-visible");
+
+    setTimeout(() => {
+      addedMessage.classList.remove("added-to-cart-visible");
+    }, 2000);
   });
 });
