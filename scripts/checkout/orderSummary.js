55765450dd2b17e05e463b85ace0c6d3f89d7794
phyominpaing <minpaingphyo255@gmail.com>
2025-01-30 00:38:18 +0630

    Day 11 Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,10 +5,10 @@ import {
     updateQuantity,
     updateDeliveryOption,
   } from "../../data/cart.js";
-  import { products } from "../../data/products.js";
+  import { products, getProduct } from "../../data/products.js";
   import { formatCurrency } from "../utils/money.js";
   import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-  import { deliveryOptions } from "../../data/deliveryOption.js";
+  import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
   
   const today = dayjs();
   const deliveryDate = today.add(7, "days");
@@ -21,22 +21,11 @@ import {
       const productId = cartItem.productId;
       const quantity = cartItem.quantity;
   
-      let matchingProduct;
-  
-      products.forEach((product) => {
-        if (product.id === productId) {
-          matchingProduct = product;
-        }
-      });
+      const matchingProduct = getProduct(productId);
   
       const deliveryOptionId = cartItem.deliveryOptionId;
-      let deliveryOption;
-  
-      deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-          deliveryOption = option;
-        }
-      });
+
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
   
       const today = dayjs();
       const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
