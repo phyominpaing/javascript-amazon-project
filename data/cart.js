b1565eb1668e2f9dd69e61993bb0a75038a7a082
phyominpaing <minpaingphyo255@gmail.com>
2025-02-03 11:35:01 +0630

    Day 13 testing my code using Jasmine framework

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
 
 if (!cart) {
   cart = [
@@ -14,11 +19,12 @@ if (!cart) {
     },
   ];
 }
+};
 
 function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
-
+ 
 export function addToCart(productId, quantity) {
   let matchingItem;
 
